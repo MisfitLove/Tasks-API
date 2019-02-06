91b43de855db81040820a3406e330113f2d54dd6
MisfitLove <30420775+MisfitLove@users.noreply.github.com>
2019-02-06 11:23:19 +0100

    basic routing

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 
 <?php
+
+// $request = $_SERVER['REDIRECT_URL'];
+
+// switch ($request) {
+//     case '/' :
+//         require __DIR__ . '/views/index.php';
+//         break;
+//     case '' :
+//         require __DIR__ . '/views/index.php';
+//         break;
+//     case '/about' :
+//         require __DIR__ . '/views/about.php';
+//         break;
+//     default: 
+//         require __DIR__ . '/views/404.php';
+//         break;
+// }
+
+
 echo "Nothing to see here";
 
 ?>
