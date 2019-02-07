b29b1e768c3c522745245b8e45b17532b220ba14
MisfitLove <30420775+MisfitLove@users.noreply.github.com>
2019-02-07 15:17:13 +0100

    describing endpoints in index.php

--- index.php
+++ index.php
@@ -20,32 +20,35 @@
 
 $url = __DIR__ ;
 
-echo "$url/api/units/read.php - array unitów
-[
-	{
-		\"id\" : string id
-		\"name\" string
-		\"description\": string
-	}
-]
-
-https://$url/api/tasks/read_unit.php?unit_id={id} - array tasków dla unita
-[
-	{
-		\"id\" : id
-		\"name\": string
-		\"description\": string
-	}
-]
-
-https://$url/api/pictures/read_task.php?task_id={id} - array obrazków dla taska
-
-[
-	{
-		\"id\": id
-		\"word\": string
-		\"url\": string 
-	}
-]";
+echo "
+<p>
+$url/api/units/read.php - array unitów<br>
+[<br>
+	{<br>
+		\"id\" : string id,<br>
+		\"name\" string,<br>
+		\"description\": string<br>
+	}<br>
+]<br>
+</p>
+<p>
+https://$url/api/tasks/read_unit.php?unit_id={id} - array tasków dla unita<br>
+[<br>
+	{<br>
+		\"id\" : id<br>
+		\"name\": string<br>
+		\"description\": string<br>
+	}<br>
+]<br>
+</p>
+<p>
+https://$url/api/pictures/read_task.php?task_id={id} - array obrazków dla taska<br>
+[<br>
+	{<br>
+		\"id\": id<br>
+		\"word\": string<br>
+		\"url\": string <br>
+	}<br>
+]<br></p>";
 
 ?>
